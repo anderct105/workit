c3f40d012e5a58189366723451a1ec5b2edd3f06
ander <anderct105@gmail.com>
2021-02-28 11:35:09 +0100

    Proyecto creado

---
+++ app/src/main/java/ehu/das/workit/LoginFragment.java
@@ -0,0 +1,39 @@
+package ehu.das.workit;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+
+
+public class LoginFragment extends Fragment {
+
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
